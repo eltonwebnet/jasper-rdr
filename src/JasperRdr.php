8abf62b1a0ee006aa8ebe466de773b6ec318bbd9
elton.alencar <elton.alencar@fourbank.com.br>
2025-05-05 23:46:17 -0300

    ajustes

--- src/JasperRdr.php
+++ src/JasperRdr.php
@@ -21,6 +21,7 @@ class JasperRdr {
         $report_file_full_path = "$storage_full_path\\$uuid" . ($tipo == "pdf" ? ".pdf" : ".xlsx");
 
         Storage::put("relatorios\\$uuid.json", $dados);
+        dd(dirname(__FILE__));
         $render_path = "jasper-rdr-compiler.jar";
         exec('java -jar ' .$render_path. " $template $tipo $uuid $storage_full_path $json_full_path  $new_parameters_json 2>&1",$output, $result);
         $hasError = false;
